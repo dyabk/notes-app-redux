c2198e9b4111cafad1f3b5511ad9eecd6f9ad0d0
dyabk <dzmitry.yabkevich@outlook.com>
2022-05-15 19:48:22 -0400

    Installed Redux. Wrote the first version of the application that stores two notes in the store and renders them on the page

--- src/index.js
+++ src/index.js
@@ -1,17 +1,50 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createStore } from "redux";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const noteReducer = (state = [], action) => {
+  if (action.type === "NEW_NOTE") {
+    state.push(action.data);
+    return state;
+  }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  return state;
+};
+
+const store = createStore(noteReducer);
+
+store.dispatch({
+  type: "NEW_NOTE",
+  data: {
+    content: "the app state is in redux store",
+    important: true,
+    id: 1,
+  },
+});
+
+store.dispatch({
+  type: "NEW_NOTE",
+  data: {
+    content: "state changes are made with actions",
+    important: false,
+    id: 2,
+  },
+});
+
+const App = () => {
+  return (
+    <div>
+      <ul>
+        {store.getState().map((note) => (
+          <li key={note.id}>
+            {note.content} <strong>{note.important ? "important" : ""}</strong>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
