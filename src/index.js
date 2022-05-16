bf07015feccd727bc89609bb207cf44b1dd06c50
dyabk <dzmitry.yabkevich@outlook.com>
2022-05-16 00:06:34 -0400

    Added the functionality for adding new notes and changing their importance

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { createStore } from "redux";
-import noteReducer from "./noteReducer";
+import noteReducer from "./reducers/noteReducer";
 
 const store = createStore(noteReducer);
 
@@ -24,12 +24,41 @@ store.dispatch({
   },
 });
 
+const generateId = () => {
+  Number((Math.random() * 1000000).toFixed(0));
+};
+
 const App = () => {
+  const addNote = (event) => {
+    event.preventDefault();
+    const content = event.target.note.value;
+    event.target.note.value = "";
+    store.dispatch({
+      type: "NEW_NOTE",
+      data: {
+        content,
+        important: false,
+        id: generateId(),
+      },
+    });
+  };
+
+  const toggleImportance = (id) => {
+    store.dispatch({
+      type: "TOGGLE_IMPORTANCE",
+      data: { id },
+    });
+  };
+
   return (
     <div>
+      <form onSubmit={addNote}>
+        <input name="note" />
+        <button type="submit">add</button>
+      </form>
       <ul>
         {store.getState().map((note) => (
-          <li key={note.id}>
+          <li key={note.id} onClick={() => toggleImportance(note.id)}>
             {note.content} <strong>{note.important ? "important" : ""}</strong>
           </li>
         ))}
