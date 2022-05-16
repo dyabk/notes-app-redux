6ead8e6372876d022dfae9f08648c2496b5e8f56
dyabk <dzmitry.yabkevich@outlook.com>
2022-05-16 02:01:40 -0400

    Moved the action creators into the reducer module. Refactored the App component into its own module. Installed react-redux. Forwarded the store to a React component with the help of the useDispatch- hook

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+
 import { createStore } from "redux";
+import { Provider } from "react-redux";
+import "./index.css";
+import App from "./App";
 import noteReducer from "./reducers/noteReducer";
 
 const store = createStore(noteReducer);
@@ -24,56 +27,9 @@ store.dispatch({
   },
 });
 
-const createNote = (content) => {
-  return {
-    type: "NEW_NOTE",
-    data: {
-      content,
-      important: false,
-      id: generateId(),
-    },
-  };
-};
-
-const toggleImportanceOf = (id) => {
-  return {
-    type: "TOGGLE_IMPORTANCE",
-    data: { id },
-  };
-};
-
-const generateId = () => {
-  Number((Math.random() * 1000000).toFixed(0));
-};
-
-const App = () => {
-  const addNote = (event) => {
-    event.preventDefault();
-    const content = event.target.note.value;
-    event.target.note.value = "";
-    store.dispatch(createNote(content));
-  };
-
-  const toggleImportance = (id) => {
-    store.dispatch(toggleImportanceOf(id));
-  };
-
-  return (
-    <div>
-      <form onSubmit={addNote}>
-        <input name="note" />
-        <button type="submit">add</button>
-      </form>
-      <ul>
-        {store.getState().map((note) => (
-          <li key={note.id} onClick={() => toggleImportance(note.id)}>
-            {note.content} <strong>{note.important ? "important" : ""}</strong>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-};
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
