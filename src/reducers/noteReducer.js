fb491db9824c66676b7c28b7d669fd6673db0a54
dyabk <dzmitry.yabkevich@outlook.com>
2022-05-22 00:16:16 -0400

    Created the second filter and combined them into the actual reducer for the application

--- src/reducers/noteReducer.js
+++ src/reducers/noteReducer.js
@@ -1,20 +1,19 @@
-const initialState = [
-  {
-    content: "reducer defines how redux store works",
-    important: true,
-    id: 1,
-  },
-  {
-    content: "state of store can contain any data",
-    important: false,
-    id: 2,
-  },
-];
+const initialState = {
+  notes: [
+    {
+      content: "reducer defines how redux store works",
+      important: true,
+      id: 1,
+    },
+    { content: "state of store can contain any data", important: false, id: 2 },
+  ],
+  filter: "IMPORTANT",
+};
 
 const noteReducer = (state = initialState, action) => {
   switch (action.type) {
     case "NEW_NOTE":
-      return [...state, action.data];
+      return [...state.notes, action.data];
     case "TOGGLE_IMPORTANCE":
       const id = action.data.id;
       const noteToChange = state.find((n) => n.id === id);
